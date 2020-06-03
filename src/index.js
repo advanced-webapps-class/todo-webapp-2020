5aab6511668799d242622616b03d9291684625d5
AJ <aj.nice@kakaocorp.com>
2020-06-03 13:42:10 +0900

    delete 버튼 캡쳐

--- src/index.js
+++ src/index.js
@@ -4,6 +4,13 @@ import inputForm from './input-form';
 import './todos.css';
 
 const todos = [];
+const $result = document.querySelector('#result');
+
+$result.addEventListener('click', (event) => {
+  if (event.target.className === 'delete') {
+    console.log('delete');
+  }
+});
 
 inputForm.init(todos);
 render(todos);
