b14eeac4db721c027384d7218d2ca050c445c170
AJ <aj.nice@kakaocorp.com>
2020-06-03 13:02:07 +0900

    submit이벤트에서 input value 가져오기

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './todos.css';
+
+const $todoForm = document.querySelector('#todo-form');
+const $input = document.querySelector('#input');
+
+$todoForm.addEventListener('submit', (event) => {
+  console.log($input.value);
+  event.preventDefault();
+});
