e4b407573b31959c250f17c92063e9e8b57c452e
AJ <aj.nice@kakaocorp.com>
2020-06-03 13:10:38 +0900

    todos데이터로 html렌더링 하기

--- src/index.js
+++ src/index.js
@@ -2,8 +2,38 @@ import './todos.css';
 
 const $todoForm = document.querySelector('#todo-form');
 const $input = document.querySelector('#input');
+const $result = document.querySelector('#result');
+
+const todos = [
+  {
+    id: 1,
+    text: 'js공부',
+  },
+];
+const todoModel = {
+  id: '',
+  text: '',
+};
 
 $todoForm.addEventListener('submit', (event) => {
   console.log($input.value);
   event.preventDefault();
 });
+
+function getTodosHtml(data) {
+  const html = data.map((todo) => {
+    return `<li>
+    <button class="delete">×</button>
+    <input type="checkbox" class="toggle-checked" />
+    <span class="text">${todo.text}</span>
+  </li>`;
+  });
+
+  return `<ul>${html.join('')}</ul>`;
+}
+
+function render(data) {
+  $result.innerHTML = getTodosHtml(data);
+}
+
+render(todos);
