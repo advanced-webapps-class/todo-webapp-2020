724f5385e8f3d2ef65374492a8ce9acbbabd9888
AJ <aj.nice@kakaocorp.com>
2020-06-03 13:39:42 +0900

    todo-model 모듈 생성

--- src/input-form.js
+++ src/input-form.js
@@ -1,12 +1,8 @@
 import { render } from './html-render';
+import todoModel from './todo-model';
 
 const $todoForm = document.querySelector('#todo-form');
 const $input = document.querySelector('#input');
-
-const todoModel = {
-  id: '',
-  text: '',
-};
 
 function init(data) {
   $todoForm.addEventListener('submit', (event) => {
